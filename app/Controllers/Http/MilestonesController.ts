3972309bb79dec6133a957d54453aee4b316214f
AlaeddineAzri <alaeddinazri@gmail.com>
2022-05-28 11:04:52 +0100

    TasksMilestones & Milestones Controller

--- app/Controllers/Http/MilestonesController.ts
+++ app/Controllers/Http/MilestonesController.ts
@@ -1,3 +1,44 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from '@ioc:Adonis/Core/Validator'
+import Milestone from 'App/Models/Milestone'
 
-export default class MilestonesController {}
+export default class MilestonesController {
+        
+        public async index() {
+            return Milestone.all();
+    
+        }
+    
+        public async store({request, response}: HttpContextContract) {
+            const newMilestoneSchema= schema.create({
+                name: schema.string({ trim: true }),
+                description: schema.string({ trim: true }),
+                due_date: schema.date(),
+            })
+            const payload = await request.validate({schema: newMilestoneSchema})
+            const milestone = await Milestone.create(payload)
+            response.status(201)
+            return milestone
+        }
+    
+        public async show({ params }: HttpContextContract) {
+            return Milestone.findOrFail(params.id);
+    
+        }
+    
+        public async update({ params, request }: HttpContextContract) {
+            const body = request.body()
+            const milestone = await Milestone.findOrFail(params.id)
+            milestone.merge(body)
+            return milestone.save()
+        }
+    
+        public async destroy({ params, response }: HttpContextContract) {
+            const milestone = await Milestone.findOrFail(params.id)
+            response.status(204)
+            await milestone.delete()
+            return milestone;
+        }
+    
+        
+}
--- app/Controllers/Http/TasksMilestonesController.ts
+++ app/Controllers/Http/TasksMilestonesController.ts
@@ -1,3 +1,44 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from '@ioc:Adonis/Core/Validator'
+import TaskMilestone from 'App/Models/TaskMilestone'
 
-export default class TasksMilestonesController {}
+
+
+export default class TasksMilestonesController {
+    public async index() {
+        return TaskMilestone.all();
+
+    }
+
+    public async store({request, response}: HttpContextContract) {
+        const newTaskMilestoneSchema= schema.create({
+            task_id: schema.number(),
+            milestone_id: schema.number(),
+        })
+        const payload = await request.validate({schema: newTaskMilestoneSchema})
+        const taskMilestone = await TaskMilestone.create(payload)
+        response.status(201)
+        return taskMilestone
+    }
+
+    public async show({ params }: HttpContextContract) {
+        return TaskMilestone.findOrFail(params.id);
+
+    }
+
+    public async update({ params, request }: HttpContextContract) {
+        const body = request.body()
+        const taskMilestone = await TaskMilestone.findOrFail(params.id)
+        taskMilestone.merge(body)
+        return taskMilestone.save()
+    }
+
+    public async destroy({ params, response }: HttpContextContract) {
+        const taskMilestone = await TaskMilestone.findOrFail(params.id)
+        response.status(204)
+        await taskMilestone.delete()
+        return taskMilestone;
+    }
+
+    
+}
